69123cd5b8dc7bb15158f72773ea3062a0444b56
b04902092 <b04902092@linux7.csie.ntu.edu.tw>
2017-08-18 08:52:53 +0800

    sol_v7 js_v3

---
+++ xordir/json_v3.js
@@ -0,0 +1,109 @@
+const fs = require('fs');
+var bignum = require('bignum');
+var keccak256 = require('js-sha3').keccak256;
+
+//bignum to xor json file : 155kb about 1 min
+function d2h(d) {return d.toString(16);}
+function h2d(s) {
+
+    function add(x, y) {
+        var c = 0, r = [];
+        var x = x.split('').map(Number);
+        var y = y.split('').map(Number);
+        while(x.length || y.length) {
+            var s = (x.pop() || 0) + (y.pop() || 0) + c;
+            r.unshift(s < 10 ? s : s - 10); 
+            c = s < 10 ? 0 : 1;
+        }
+        if(c) r.unshift(c);
+        return r.join('');
+    }
+
+    var dec = '0';
+    s.split('').forEach(function(chr) {
+        var n = parseInt(chr, 16);
+        for(var t = 8; t; t >>= 1) {
+            dec = add(dec, dec);
+            if(n & t) dec = add(dec, '1');
+        }
+    });
+    return dec;
+}
+function stringToHex (tmp) {
+	var str = '',i = 0,tmp_len = tmp.length,c;	 
+	for (; i < tmp_len; i += 1) {
+        	c = tmp.charCodeAt(i);
+	        str += d2h(c);
+	}
+   return str;
+}
+//config use hex.js calculate binum(nchc^conaddr)
+var bighash = bignum("21310093456092736124621884121797009578577705822150884401635626515301414610709",base=10);
+//var sha3nchc = keccak256("nchc");
+//var hexnchc = stringToHex(sha3nchc);						//config str
+//var nchc = h2d(hexnchc);
+//contract address only have 20 bytes padding to bytes32
+
+//var tmpcon = "0x000000000000000000000000f2bd5de8b57ebfc45dcee97524a7a08fccc80aef";	//config contract address
+//var conaddr = h2d(tmpcon);
+//var bignchc = bignum(nchc,base=10);
+//var bigcon = bignum(conaddr,base=10);
+//var ans = bignchc.xor(bigcon);
+fs.readFile('./test.json','utf-8', (err, data) => {
+		  if (err) throw err;
+		   // console.log(data);
+			var str = JSON.stringify(data); 
+		//	console.log(str);
+			var sha3 = keccak256(str);
+			
+			console.log(sha3);				//sha json file
+
+			var len = str.length;			//cut json
+			console.log(len);
+			var array = new Array()			
+			var i ;
+			for(i = 0; i+32 < len ; i +=32)
+			{
+				var tmp=xorjson(str.slice(i,i+32));
+				console.log(xorjson(str.slice(i,i+32)));
+				array.push("0x"+tmp);
+				if((i+32)%32000 == 0)				//config how much item to cut 
+				{
+					if((i+32)/32000 == 1)
+					//console.log(JSON.stringify(array));
+					array = [];
+				}
+				//var test = encryptDecrypt(array[i/32]);
+				//console.log(test);
+			}
+			var tmp=xorjson(str.slice(i,i+32));
+			array.push("0x"+tmp);
+			//console.log(array.length);
+			console.log(JSON.stringify(array));
+});
+
+function xorjson(str){
+   var tmp = stringToHex(str);
+   tmp = "0x"+tmp;
+   var dectmp = h2d(tmp);
+   var bigtmp = bignum(dectmp,base=10);
+   var ans = bighash.xor(bigtmp);
+   return (ans.toString(16));
+}
+// not used
+function encryptDecrypt(input) {
+	var key = ['K', 'C', 'Q']; //Can be any chars, and any size array
+	var output = [];
+	
+	for (var i = 0; i < input.length; i++) {
+		var charCode = input.charCodeAt(i) ^ key[i % key.length].charCodeAt(0);
+		output.push(String.fromCharCode(charCode));
+	}
+	return output.join("");
+}
+
+
+/*var encrypted = encryptDecrypt("idolmasteridolmasteridolmasterid");
+console.log("Encrypted:"+encrypted);
+var decrypted = encryptDecrypt(encrypted);
+console.log("Decrypted:"+decrypted);*/
---
+++ xordir/test_v7.sol
@@ -0,0 +1,219 @@
+// 2.VoteCount == 2 -> event
+// 3.professor SC => show pending student request
+// 4.use web3 api call seturl to set hashurl to structure
+// 5. after get url from web send event => graduate(encrypted)
+// problem 2:input json should xor back and hash
+//  problem3 : msg.sender = 教網中心
+// problem 4: init send json
+// problem 5: add event and oracale at last
+//xor json file added (nodejs encode for 1 min)
+
+import "github.com/Arachnid/solidity-stringutils/strings.sol";
+pragma solidity ^0.4.10;
+contract SimpleOracle {
+    // 增加owner
+    address owner;
+    // 增加responder
+    address public responder;
+    mapping(uint256 => bytes) responses;
+    
+    // 設定owner
+    function SimpleOracle() {
+        owner = msg.sender;
+    }
+
+    // owner才能使用的
+    modifier onlyOwner() {
+        if (msg.sender != owner) throw;
+        _;
+    }
+
+    // responder才能使用的
+    modifier onlyResponder() {
+        if (msg.sender != responder) throw;
+        _;
+    }
+    
+    event upload(address sender);       //verified event to let web server know
+
+    // 設定responder，並限定owner才能使用
+    function setResponder(address _responder) external onlyOwner {
+        responder = _responder;
+    }
+    
+    function query(address sender) external returns (uint256) {
+        uint256 id = uint256(sha3(block.number, now, sender, msg.sender));
+        upload(sender);
+        return id;
+    }
+    
+    // 限定responder才能使用
+    function _queryCallback(uint256 _id, bytes _response) external onlyResponder {
+        responses[_id] = _response;
+    }
+    
+    function getResponse(uint256 _id) external constant returns (bytes) {
+        return responses[_id];
+    }
+}
+contract Oracle_update {
+    address owner;
+    event upload(address sender);
+    function Oracle() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender != owner) throw;
+        _;
+    }
+
+    function query() returns (bytes32 id) {
+        id = sha3(block.number, now, msg.sender);
+        upload(msg.sender);
+        return id;
+    }
+    
+}
+contract test{
+       using strings for *;
+       address user;    //student
+       address admin;   //教網中心
+       string json;
+       bytes32 shanchc;
+
+       Oracle_update Oracle = Oracle_update(); //oracle address config
+       //if signature collected upload event
+       struct diploma{
+           bool[2] verified;
+           address[2] relate;
+           bytes32 hashjson;
+       }
+       mapping(uint => diploma) Diploma;
+    
+   // user = msg.sender !!!
+   function test(address super){
+       user = msg.sender;
+       admin = super;
+       shanchc = sha3("nchc");
+   }
+   
+   
+   // don't need return,calculate addr0 addr1
+   // address don't need to transform to bytes20(xoraddr0,xoraddr1)
+   function init(bytes20 decode,bytes20 xoraddr0,bytes20 xoraddr1){
+        if(msg.sender != admin){throw;}
+        uint i = xorint(decode);
+        //store hash
+        Diploma[i].relate[0] = address(xoraddr0);
+        Diploma[i].relate[1] = address(xoraddr1);
+        Diploma[i].verified[0]=false;
+        Diploma[i].verified[1]=false;
+   }
+   //decode sending address
+   
+   //get i xor(sha3(idolmaster), con)
+   function xorint(bytes20 num) returns(uint){
+        bytes20 str = bytes20(sha3("idolmaster"));
+        address con =this;
+        return uint(str^bytes20(con)^num);
+    }
+    
+    //nchc^relate^contract used in getting init
+    function xorrelate(uint num,bytes20 relate) returns(address ans){
+         address con = this;
+         bytes20 str;
+         if(num == 0){
+            str= bytes20(sha3("nchc"));
+            ans =  address((bytes20(con)^str^relate));
+         }
+         if(num == 1){
+            str= bytes20(sha3("nchc1"));
+            ans =  address((bytes20(con)^str^relate));
+         }        
+    }
+    
+   // verify signature verify = msg.sender  ?
+   // num and who should xor?
+   function verify(bytes20 numhex,bytes32 hash,uint8 v,bytes32 xorr,bytes32 xors){
+        if(msg.sender != admin){throw;}
+        uint num = xorint(numhex);
+        uint who=10;
+        bytes32 r =xorsign(xorr);
+        bytes32 s =xorsign(xors);
+        
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHash = sha3(prefix, hash);
+        address tmp = ecrecover(prefixedHash, v, r, s);
+        
+        address addr0 = xorrelate(0,bytes20(Diploma[num].relate[0]));
+        if(tmp == addr0) who=0;
+        else{
+            addr0 = xorrelate(1,bytes20(Diploma[num].relate[1]));
+            if(tmp == addr0) who=1;
+        }
+        
+        if(who ==10 || Diploma[num].verified[who] == true){
+            throw;
+        }
+        Diploma[num].verified[who] = true;
+        if(Diploma[num].verified[0] == true && Diploma[num].verified[1] == true){
+            Oracle.query();
+        }
+    }
+    
+    //unxor sign(r,s)
+    function xorsign(bytes32 xorr) returns(bytes32 r){
+        address con = this;
+        bytes32 str="nchcnchcnchcnchcnchcnchcnchcnchc";
+        r = bytes32(con)^str^xorr;
+    }
+    
+    function trace(bytes20 hexnum) returns(bool,bool){
+        uint num=xorint(hexnum);
+        return(Diploma[num].verified[0],Diploma[num].verified[1]);
+    }
+    
+    function bytes32ArrayToString (bytes32[] data) returns (string) {
+        bytes memory bytesString = new bytes(data.length * 32);
+        uint urlLength;
+        for (uint i=0; i<data.length; i++) {
+            for (uint j=0; j<32; j++) {
+                byte char = byte(bytes32(uint(data[i]) * 2 ** (8 * j)));
+                if (char != 0) {
+                    bytesString[urlLength] = char;
+                    urlLength += 1;
+                }
+            }
+        }
+            return string(bytesString);
+    }
+    function dexorjson(bytes32 a,bytes32 halfxor) returns(bytes32){
+        return (a^halfxor);
+    }
+
+    function setjson(bool check,bytes32[] a) {
+        if(check == true){
+            address conaddr = this;
+            bytes32 halfxor = bytes32(conaddr)^shanchc;
+
+            uint len = a.length;
+            for(uint i = 0 ;i < len; i++){
+                a[i] = dexorjson(a[i],halfxor);
+            }
+            json =json.toSlice().concat((bytes32ArrayToString(a)).toSlice());
+        }
+        else{
+            uint num = xorint(bytes20(a[0]));
+            Diploma[num].hashjson = sha3(json);
+            json = "";
+        }
+    }
+    //student give a string^ conaddr^nchc
+    // return num test
+    function getjson(bytes20 hexnum) returns(bytes32){
+        uint num = xorint(hexnum);
+        return (Diploma[num].hashjson);
+    }
+ }
+ 
